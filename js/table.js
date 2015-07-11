2e6d97d2026bc0f4d53fe8c93ce181a23a5a3615
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2015-07-11 18:02:03 +0300

    Initial commit. Skeleton for json and some testing with table generation.

---
+++ js/table.js
@@ -0,0 +1,27 @@
+function createTable() {
+  var x = document.tablecreator.x.value;
+  var y = document.tablecreator.y.value;
+    
+  var table = document.getElementById("spacegrid");
+  
+  while(table.firstChild)
+  {
+    table.removeChild(table.firstChild);
+  }
+  
+  test = document.createElement("tbody");
+  table.appendChild(test);
+
+
+  for ( var i = 0; i < y; i++) {
+    
+    var row = document.createElement("tr");
+    
+    for ( var e = 0; e < x; e++) {
+        var td = document.createElement("td");
+        row.appendChild(td);
+    }
+    
+    table.appendChild(row);
+  }
+}
