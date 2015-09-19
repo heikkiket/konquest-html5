32622a2f47f27af0222afd2042eb863d1c7e2813
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2015-09-20 01:12:33 +0300

    Moved code from planets.js and table.js to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Konquest Scripts.js -file
-// This contains all the app Java Script stuff
+// This contains all the Java Script stuff written for app
 
 //-------------- Before the game -------------------------------
 
@@ -13,6 +13,12 @@ function initGame()
 function initSpace()
 {
   // This will randomize space and return it to a preview screen.
+
+  function createPlanet() {
+    //This randomizes a single planet. Maybe one could give parameters
+    //to instruct what kind of planet it will be. parameters could be
+    //in key=>value array.
+  }
 }
 
 function initScreen()
@@ -20,7 +26,7 @@ function initScreen()
   //This kind of helper function may be necessary for Init screen
 }
 
-//------------------ Between turns ------------------------------
+//------------------ In every turn ------------------------------
 
 function updateJSON()
 {
@@ -37,7 +43,7 @@ function newTurn()
   // If in remote mode, upload JSON, combine in server and download
   //-------------------
   
-  //Check for fights and do them
+  //Check for fights and do them ... in which order?
   
   //Change planet ownerships
   
@@ -47,20 +53,80 @@ function newTurn()
   //Alerts / messages
   
   //Draw planets
+  drawGrid();
+  drawPlanets();
+  
+}
+
+function drawGrid() {
+  var x = document.tablecreator.x.value;
+  var y = document.tablecreator.y.value;
+  
+  var grid = document.getElementById("spacegrid");
+  
+  //empty grid
+  while(grid.firstChild)
+  {
+    grid.removeChild(grid.firstChild);
+  }
+  
+  
+  for ( var i = 0; i < y; i++) {
+    
+    var row = document.createElement("tr");
+    
+    for ( var e = 0; e < x; e++) {
+      var td = document.createElement("td");
+      row.appendChild(td);
+    }
+    
+    table.appendChild(row);
+  }
+  
+}
+
+function drawPlanets() {
+  
+  $.each(konquestData.planets, function(key, planet) {
+    
+    //lets draw a planet with the help of jQuery selectors
+    
+    $( "#spacegrid tr:nth-child(" + planet.y +
+    ") td:nth-child(" + planet.x + ")" )
+    .append('<span data-tooltip aria-haspopup="true" class="has-tip" title="'
+    + '<b>Name:</b> ' + planet.name + '<br /><b>Ships:</b> ' + planet.ships + '<br /><b>Kill percent:</b> ' + planet.killPercent
+    + '">'
+    + '<img src="img/planet1.png"'
+    + '" onmouseover="showPlanetInfo(\''+key+'\')" onmouseout="hidePlanetInfo();" /></span>');
+  });
+  
+  //Ask Foundation to redraw tooltips
+  $(document).foundation('tooltip', 'reflow');
   
 }
 
 //------------------ During the game -------------------------------
 
-function showPlanetInfo()
-{
-  //Already implemented
+function showPlanetInfo(planetId) {
+  
+  $( "#planetinfo .name" ).html("<b>Name:</b> " + konquestData.planets[planetId].name);
+  $( "#planetinfo .killPercent" ).html("<b>Kill percent:</b> " + konquestData.planets[planetId].killPercent);
+  $( "#planetinfo .productionRate" ).html("<b>Production rate:</b> " + konquestData.planets[planetId].productionRate);
+  $( "#planetinfo .ships" ).html("<b>Amount of ships:</b> " + konquestData.planets[planetId].ships);
+  
+  
+  $( "#planetinfo" ).show();
+}
+
+function hidePlanetInfo() {
+  $( "#planetinfo" ).hide();
 }
 
 //TODO: Fleet launching needs a gui and maybe some helper functions also.
 function launchFleet(planetA, planetB, player)
 {
-  // TODO: This function launches a fleet from planet A to planet B
+  // TODO: This function launches a fleet from planet A to planet B,
+  // controlled by player.
 }
 
 function showStandings()
@@ -75,4 +141,4 @@ function backgroundStuff() {
 //------------------- GUI helpers ------------------------------------
 
 //Maybe we should group all the helper functions here? Like Toggle elements,
-//button handlers etc.
+//button handlers etc. Not sure if there will be many.
